06a560d5d64352615a96d2fcc064dea73b882cba
anna kisel <>
2025-03-29 15:12:23 +0300

    add trim of name and password to registration and authorisation dto

--- src/main/java/com/transport/api/dto/user/AuthenticationDto.java
+++ src/main/java/com/transport/api/dto/user/AuthenticationDto.java
@@ -1,17 +1,33 @@
 package com.transport.api.dto.user;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthenticationDto {
+
+    @NotBlank
+    @Size(min = 1)
+    @Pattern(regexp = "^(?!\\s).*[^\\s]$", message = "Не должно быть пробелов до или после имени")
     private String name;
+    @NotBlank
+    @Size(min = 1)
+    @Pattern(regexp = "^(?!\\s).*[^\\s]$", message = "Не должно быть пробелов до или после пароля")
     private String password;
+
+    public void setName(String name) {
+        this.name = name != null ? name.trim() : null;
+    }
+
+    public void setPassword(String password) {
+        this.password = password != null ? password.trim() : null;
+    }
 }
--- src/main/java/com/transport/api/dto/user/RegistrationUserDto.java
+++ src/main/java/com/transport/api/dto/user/RegistrationUserDto.java
@@ -3,6 +3,8 @@ package com.transport.api.dto.user;
 import java.util.Set;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,8 +19,12 @@ import lombok.Setter;
 public class RegistrationUserDto {
 
     @NotBlank
+    @Size(min = 1)
+    @Pattern(regexp = "^(?!\\s).*[^\\s]$", message = "Не должно быть пробелов до или после имени")
     private String name;
     @NotBlank
+    @Size(min = 1)
+    @Pattern(regexp = "^(?!\\s).*[^\\s]$", message = "Не должно быть пробелов до или после пароля")
     private String password;
     @NotBlank
     @Email(regexp = ".+[@].+[\\.].+")
@@ -31,4 +37,12 @@ public class RegistrationUserDto {
     private String phoneNumber;
     private String about;
     private Set<RoleDto> roles;
+
+    public void setName(String name) {
+        this.name = name != null ? name.trim() : null;
+    }
+
+    public void setPassword(String password) {
+        this.password = password != null ? password.trim() : null;
+    }
 }
