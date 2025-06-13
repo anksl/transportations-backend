e7b37e91786647e3b41c60b9cb9be1873d1d8dbe
anna kisel <>
2025-06-13 16:05:39 +0300

    disable actuator

--- src/main/java/com/transport/TransportApplication.java
+++ src/main/java/com/transport/TransportApplication.java
@@ -6,9 +6,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@SpringBootApplication(exclude = {
-    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableSpringDataWebSupport
 public class TransportApplication {
     public static void main(String[] args) {
